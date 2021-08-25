c054cde93d9d65b775555b7a2271b8732540eecc
Saurav222 <tcsSaurav123@gmail.com>
2021-08-25 11:51:24 +0530

    Exception Handling with correct Status Code

--- src/main/java/com/tcs/springbootdemo/UserController.java
+++ src/main/java/com/tcs/springbootdemo/UserController.java
@@ -1,7 +1,13 @@
 package com.tcs.springbootdemo;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +21,16 @@ public class UserController { // spring bean, act as request receiver
 	@GetMapping("/user")
 	private Iterable<User> getUser() {
 		return userService.getAllUsers();
+	}
+
+	@GetMapping("/user/{id}")
+	private Optional<User> getUser(@PathVariable("id") Integer id) {
+		return userService.getUser(id);
+	}
+
+	@ExceptionHandler(value = UserNotFoundException.class)
+	public ResponseEntity<User> exception(UserNotFoundException userNotFoundException) {
+		return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
 	}
 
 	@PostMapping("/user")
